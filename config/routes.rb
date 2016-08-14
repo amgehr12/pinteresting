9967d38cf671c907aa4a2cca0677291d3fb5291a
Audrey <audreygehring@gmail.com>
2016-08-14 18:28:12 -0400

    Implemented Devise for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates about_path
-  get "login" => "pages#login" # creates login_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
